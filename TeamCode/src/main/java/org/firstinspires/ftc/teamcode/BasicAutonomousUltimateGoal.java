b1333614db3370cea2c5b454f9a994130cd6689a
TonyDangermouse <ruby.rubick@gmail.com>
2021-02-19 20:10:00 -0800

    don't merge branches please and thank you

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAutonomousUltimateGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAutonomousUltimateGoal.java
@@ -173,41 +173,6 @@ public class BasicAutonomousUltimateGoal extends LinearOpMode
 >>>>>>> parent of fac1a7a... Added for loop to reload and shoot the rings on the floor. Also added code to drive the robot over the line for an extra 5 points.
         //Saves the robot's position and heading
         HardwareUltimateGoal.writePositionHeading(posTarMan.getRobotPosition(), posTarMan.getRobotHeading());
-
-        //---------------------------------------------------------------------------------------\\
-        /* new idea
-        (while doing these steps, make sure to update the robots position frequently)
-
-        step 1: move forward to right before the launch line, in front of the starting stack, without knocking them over
-
-        step 2: fire the 2 rings we preloaded (either the high goal, or the power shots, we need to test the robots accuracy to figure out which is optimal
-
-        step 3: move to the starting stack, and reload 1 ring from the starting stack
-
-        step 4: move back to (right before) the launch line (optional), and fire the ring we just picked up
-
-        step 5: (if we figure out that we'll have time, and be able to (if the stack gets scattered too much in step 4 this won't be possible), repeat steps 3-4 until we have fired 3 rings from the starting stack
-
-        step 6: move ON TOP OFF the launch line, and end autonomous, (in the end step, make sure to accurately store the robots position and heading using the writePositionHeading(double[] position, double heading) method of the HardwareUltimateGoal.java class
-         */
-
-        /* old:
-        step 1: pick up and store the rings in the starter stack
-            (if we get a way to know how many rings where in the starter stack, do this before doing step 2)
-            step 1.5: wobble goal stuff
-        step 2: turn parallel to goal tower, and move until aligned with the goal tower, then turn to face the goal tower
-
-            if we have a color sensor, do step 3 b, otherwise do step 3 a
-        step 3.a: use the position manager to find the launch line, then move forward until the robot as close as it can get to the launch line without moving over it
-        step 3.b: move forward until color sensor finds launch line, then stop, and reverse so the robot is no longer over launch line
-
-        step 4: using the aim manager, cycle through the targets (top to bottom) until it finds a target it can hit
-            (if it can't hit any, move forward over the launch line, and fire into the low goal
-        step 5: go through the launch cycle, with a small pause between launches to give the flywheels time to get to speed
-            repeat step 5 once per ring in the starter stack, if we don't get a way to count that, then just fire 3 times
-
-        step 6: move the robot over the launch line (on top of launch line), and save the robots heading and position
-         */
     }
     //TODO: eventually make a program that lets the robot move in arcs or something
     /**
